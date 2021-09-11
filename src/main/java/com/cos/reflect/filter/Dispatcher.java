fd373d3e566552644169bcf053a3d28146304524
sehwan08 <sehwan_8@naver.com>
2021-09-11 20:52:15 +0900

    리플렉션 어노테이션 사용까지!

--- src/main/java/com/cos/reflect/filter/Dispatcher.java
+++ src/main/java/com/cos/reflect/filter/Dispatcher.java
@@ -1,18 +1,24 @@
 package com.cos.reflect.filter;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cos.reflect.filter.anno.RequestMapping;
 import com.cos.reflect.filter.controller.UserController;
 
 public class Dispatcher implements Filter{
+	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain arg2)
 			throws IOException, ServletException {
@@ -28,10 +34,22 @@ public class Dispatcher implements Filter{
 		System.out.println("엔드포인트-> "+endPoint);
 		
 		UserController userController = new UserController();
-		if(endPoint.equals("/join")) {
-			userController.join();
-		} else if(endPoint.equals("/login")) {
-			userController.login();
+		Method[] methods = userController.getClass().getDeclaredMethods();// 그 파일에 메서드만!!
+		for (Method method : methods) {
+			Annotation annotation = method.getDeclaredAnnotation(RequestMapping.class);
+			RequestMapping requestMapping = (RequestMapping)annotation;
+			System.out.println(requestMapping.value());
+			
+			if(requestMapping.value().equals(endPoint)) {
+				try {
+					String path = (String)method.invoke(userController);
+					RequestDispatcher dis = request.getRequestDispatcher(path);
+					dis.forward(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			}
 		}
 	}
 }
--- src/main/java/com/cos/reflect/filter/controller/UserController.java
+++ src/main/java/com/cos/reflect/filter/controller/UserController.java
@@ -1,11 +1,34 @@
 package com.cos.reflect.filter.controller;
 
+import com.cos.reflect.filter.anno.RequestMapping;
+
 public class UserController {
-	public void join() {
+	
+	@RequestMapping("/join")
+	public String join() {
 		System.out.println("join() 함수 호출 됨");
+		System.out.println("----------------");
+		return "/";
 	}
 	
-	public void login() {
+	@RequestMapping("/login")
+	public String login() {
 		System.out.println("login() 함수 호출 됨");
+		System.out.println("----------------");
+		return "/";
+	}
+	
+	@RequestMapping("/user")
+	public String user() {
+		System.out.println("user() 함수 호출 됨");
+		System.out.println("----------------");
+		return "/";
+	}
+	
+	@RequestMapping("/register")
+	public String register() {
+		System.out.println("register() 함수 호출 됨");
+		System.out.println("----------------");
+		return "/register.jsp";
 	}
 }
